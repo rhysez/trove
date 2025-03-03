039e8f9890963525fb896a2db25389acafc6ef3f
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-03-03 17:04:05 +0000

    add comment for exception

--- src/sort.cpp
+++ src/sort.cpp
@@ -17,24 +17,38 @@ void sort_files(std::string path) {
     std::filesystem::path application_path = path + DIR_NAME_APPLICATION;
     std::filesystem::path misc_path = path + DIR_NAME_MISC;
 
+    bool images_exists = std::filesystem::exists(images_path);
+    bool documents_exists = std::filesystem::exists(documents_path);
+    bool audio_exists = std::filesystem::exists(audio_path);
+    bool video_exists = std::filesystem::exists(video_path);
+    bool archives_exists = std::filesystem::exists(archives_path);
+    bool fonts_exists = std::filesystem::exists(fonts_path);
+    bool application_exists = std::filesystem::exists(application_path);
+    bool misc_exists = std::filesystem::exists(misc_path);
 
     // Iterates through each entry in the directory.
     // Takes a reference to the entry, which is of type 'auto' (automatically assigned).
     for (const auto& entry : std::filesystem::directory_iterator(path)) {
-        // The file extension of the current file we want to sort.
+
         std::filesystem::path file_extension = entry.path().extension();
+        std::filesystem::path file_name = entry.path().filename();
 
         // Iterates through every vector of extensions
         for (const std::vector<std::string>& extension_vec : extensions_all) {
             // Iterates through every extension within the current extension vector
             for (const std::string& extension : extension_vec) {
-                // If the file extension matches the current extension in the loop,
-                // Check if the appropriate directory exists.
-                // If it does, move it there.
-                // Else, create the directory and then move it there.
+                // If the file extension exists in category
                if (std::find(extensions_images.begin(), extensions_images.end(), file_extension) != extensions_images.end()) {
-                   std::cout << file_extension << '\n';
-
+                   // If the images directory exists
+                   if (images_exists) {
+                       // Move to images directory
+                       // TODO: EXCEPTION THROWN HERE
+                       std::filesystem::rename(entry.path(), path + DIR_NAME_IMAGES + file_name.string());
+                   } else {
+                       // Create images directory, then move
+                       std::filesystem::create_directory(path + DIR_NAME_IMAGES);
+                       std::filesystem::rename(entry.path(), path + DIR_NAME_IMAGES + file_name.string());
+                   }
                }
             };
         }
